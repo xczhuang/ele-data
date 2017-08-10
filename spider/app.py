acd3a3154630c2c7b194d7972328495a9563b933
zhuangxinchuang <408701391@qq.com>
2017-08-10 09:48:23 +0800

    8/10

--- spider/app.py
+++ spider/app.py
@@ -4,10 +4,13 @@ import MySQLdb
 
 class  ViewData(object):
     time=input("请输入查询时间：")
-    area=input("请输入查询地区：")
-    
+    area=raw_input("请输入查询地区：")
+    def __init__(self):
+        self.tim="2001年1月"
+        self.are="安徽省"
+        self.select_data(self.tim,self.are)
 
-    def select_data(self,time,area):
+    def select_data(self,tim,are):
         conn=MySQLdb.connect(host='localhost',
                 port=3306,
                 user='root',
@@ -16,8 +19,27 @@ class  ViewData(object):
                 charset='utf8'
                 )
         cur=conn.cursor()
-        for i in xrange(1,13):
-            month_data=cur.execute("select time,area,value from ele_data where area=%s AND time=%s",(self.area,self.time+"年"+str(i)+"月"))
+        #for i in xrange(1,13):
+        times="2011年3月"
+        areas="安徽省"
+        sql1="select * from ele_data where area=\"安徽省\" AND time=\"2001年3月\""
+        print sql1
+        sql2="select * from ele_data where area="+"'"+areas+"'"+ "AND time="+"'"+times+"'"
+        print sql2
+        sql="select * from ele_data "
+        cur.execute(sql2)
+        month_datas=cur.fetchall()
+        for month_data in month_datas:
+            print month_data
+
         cur.close()
         conn.commit()
         conn.close()
+
+
+
+
+if __name__=='__main__':
+    viewdata=ViewData()
+
+
